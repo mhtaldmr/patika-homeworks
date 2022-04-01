c5361d14a80badb7639a241b01bf7ca08917e4d7
Muhammet Ali Demir <56696870+mhtaldmr@users.noreply.github.com>
2022-04-01 23:26:34 +0300

    Add files via upload

---
+++ Metotlar/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Metotlar
+            int a = 2;
+            int b = 3;
+            int c = a + b;
+
+            Console.WriteLine("a+b={0}", c);
+            Console.WriteLine("*************************");
+
+            int d = Topla(a, b);
+
+            Console.WriteLine("a+b={0}", d);
+
+            Metotlar metotlar = new Metotlar();
+            metotlar.EkranaYazdır("Merhaba");
+
+        }
+        static int Topla(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    class Metotlar
+    {
+        public void EkranaYazdır(string yazı)
+        {
+            Console.WriteLine(yazı);
+        }
+    }
+}
