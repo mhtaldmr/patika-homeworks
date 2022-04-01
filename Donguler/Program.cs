47d2eb3c3e6fcffe9ef726e4d0c318bc07900f3f
Muhammet Ali Demir <56696870+mhtaldmr@users.noreply.github.com>
2022-04-01 19:11:56 +0300

    Add files via upload

---
+++ Donguler/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int toplam = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                toplam += i;
+            }
+
+            Console.WriteLine(toplam);
+
+
+            foreach (int i in Enumerable.Range(0, 10))
+            {
+                toplam += i;
+            }
+
+            Console.WriteLine(toplam);
+
+            int[] dizi = new int[10];
+
+            for (int i = 0; i < dizi.Length; i++)
+            {
+                dizi[i] = i;
+            }
+
+            Console.WriteLine(dizi.Sum());
+            Console.WriteLine(dizi.Where(x => x % 5 == 0).Count());
+
+            var n = new String('#', 3).PadLeft(10, '*');
+
+            Console.WriteLine(n);
+
+        }
+    }
+}
