397beb0ab830c0746e4d1a778e687968d1b9b190
Muhammet Ali Demir <56696870+mhtaldmr@users.noreply.github.com>
2022-04-01 15:57:55 +0300

    Add files via upload

---
+++ Condition/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Condition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = Convert.ToInt32(DateTime.Now.ToString("hh"));
+            Console.WriteLine(time);
+            if (time < 10)
+                Console.WriteLine("Goodmorning  World!" + "Saat : " + time);
+            else if (time < 20)
+                Console.WriteLine("Goodday  World!" + "Saat : " + time);
+            else
+                Console.WriteLine("Goodevening  World!" + "Saat : " + time);
+
+        }
+    }
+}
