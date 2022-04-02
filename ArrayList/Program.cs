df2b1f2f63d1b260f846df8d6b9d4e42af5c9995
Muhammet Ali Demir <56696870+mhtaldmr@users.noreply.github.com>
2022-04-02 14:07:36 +0300

    Add files via upload

---
+++ ArrayList/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+
+namespace ArrayListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arraylist
+
+            ArrayList liste = new ArrayList();
+
+            liste.Add(1);
+            liste.Add(2);
+            liste.Add(3);
+            liste.Add(true);
+            liste.Add("Ali");
+            liste.Add(new DateTime(2019, 1, 1));
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("***************************");
+
+            liste.AddRange(new int[] { 4, 5, 6 });
+
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("***************************");
+
+            Console.WriteLine(liste.Count);
+            // Console.WriteLine(liste.Sort); //Hata verir. Çünkü arraylist icindekileri neyi sıralamak istediğinizi bilmiyor.
+            // Console.WriteLine(liste.BinarySearch(2));
+
+            liste.Reverse();
+            foreach (var item in liste)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
