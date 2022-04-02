ca46d23820c49d5a2ce4fa935ad506edef575397
Muhammet Ali Demir <56696870+mhtaldmr@users.noreply.github.com>
2022-04-02 17:46:59 +0300

    Add files via upload

---
+++ EnumTipleri/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EnumTipleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazartesi);
+
+            Console.WriteLine((int)Gunler.Pazar); //26
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi = 5,
+        Sali,
+        Carsamba,
+        Persembe,
+        Cuma,
+        Cumartesi = 25,
+        Pazar
+    }
+}
