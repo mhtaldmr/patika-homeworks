1576d60667caccdfa56d9f39816056be04cce890
Muhammet Ali Demir <56696870+mhtaldmr@users.noreply.github.com>
2022-04-02 15:52:40 +0300

    Add files via upload

---
+++ KoleksiyonSoru1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace KoleksiyonSoru1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("20 Tane sayı giriniz:");
+            int N = 0;
+            var list = new List<int>();
+            ArrayList asalSayilar = new ArrayList();
+            ArrayList asalOlmayanSayilar = new ArrayList();
+
+            try
+            {
+                while (N < 20)
+                {
+                    int sayi = Convert.ToInt32(Console.ReadLine());
+                    if (sayi > 0)
+                        list.Add(sayi);
+                    else
+                    {
+                        Console.WriteLine("Lütfen pozitif sayılar giriniz.");
+                        break;
+                    }
+                    N++;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Pozitif Sayı girmediniz! " + e.Message);
+            }
+
+
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (list[i] == 2)
+                    asalSayilar.Add(list[i]);
+                else if (list[i] == 1)
+                    asalOlmayanSayilar.Add(list[i]);
+                else if (list[i] % 2 == 0)
+                    asalOlmayanSayilar.Add(list[i]);
+                else
+                {
+                    bool asalMi = true;
+                    for (int j = 3; j < list[i] / 2; j += 2)
+                    {
+                        if (list[i] % j == 0)
+                        {
+                            asalOlmayanSayilar.Add(list[i]);
+                            asalMi = false;
+                            break;
+                        }
+                    }
+                    if (asalMi)
+                        asalSayilar.Add(list[i]);
+                }
+            }
+
+            Console.WriteLine("Asal Sayılar: ");
+            foreach (var item in asalSayilar)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Asal Olmayan Sayılar: ");
+            foreach (var item in asalOlmayanSayilar)
+            {
+                Console.Write(item + " ");
+            }
+
+
+
+
+
+        }
+    }
+}
